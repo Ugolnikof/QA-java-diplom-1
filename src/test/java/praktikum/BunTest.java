8c941bb68035def75be7adf074af96ae481d2c25
Mikhail Ugolnikov <mikhail.ugolnikov@ural.rt.ru>
2024-06-03 00:00:40 +0500

    Добавлен тест BunTest для класса Bun

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,26 @@
+package praktikum;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class BunTest {
+    private Bun bun;
+
+    @Before
+    public void setUp() {
+        bun = new Bun("Флюоресцентная булка R2-D3", 988);
+    }
+
+    @Test
+    public void getName() {
+        String actual = bun.getName();
+        assertEquals("Флюоресцентная булка R2-D3", actual);
+    }
+
+    @Test
+    public void getPrice() {
+        float actual = bun.getPrice();
+        assertEquals(988, actual, 0);
+    }
+}
