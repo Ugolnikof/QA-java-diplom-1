b037925240588c1ba68979b6e43443ec039895df
Mikhail Ugolnikov <mikhail.ugolnikov@ural.rt.ru>
2024-06-05 10:09:32 +0500

    Добавлен параметризованный тест для IngredientType

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,18 +1,31 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class IngredientTypeTest {
+    private final IngredientType type;
+    private final IngredientType name;
 
-    @Test
-    public void valueOfSauce() {
-        assertEquals(IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
+    public IngredientTypeTest(IngredientType type, IngredientType name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] setUp() {
+        return new Object[][] {
+                {IngredientType.SAUCE, IngredientType.valueOf("SAUCE")},
+                {IngredientType.FILLING, IngredientType.valueOf("FILLING")}
+        };
     }
 
     @Test
-    public void valueOfFilling() {
-        assertEquals(IngredientType.FILLING, IngredientType.valueOf("FILLING"));
+    public void valueOfSauce() {
+        assertEquals(type, name);
     }
 }
